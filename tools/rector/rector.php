29da41bf98e7c356302e9bf543b592d7d84c79b3
fezfez <demonchaux.stephane@gmail.com>
2026-03-02 10:44:53 +0100

    fix #77
    Signed-off-by: fezfez <demonchaux.stephane@gmail.com>
    
    correct psalm config path
    
    tmp

---
+++ tools/rector/rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+require_once 'vendor/autoload.php';
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPhpSets(php82: true)
+    ->withPaths([
+        // __DIR__ . '/../../src', excluded in 3.x.x branches
+        __DIR__ . '/../../test',
+    ])
+    ->withPreparedSets(
+        typeDeclarations: true,
+    );
